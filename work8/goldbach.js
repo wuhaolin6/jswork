17d8168dda7841d3b9e77cd3db1d2724a1f780e2
wuhaolin6 <364641104@qq.com>
2019-10-24 09:54:19 +0800

    work8

---
+++ work8/goldbach.js
@@ -0,0 +1,22 @@
+let a = parseInt(document.getElementById('odd').value)
+function goldbach(a){
+    var sum=0;
+    for(var i=1;i<=alert;i++){
+        if(a%i==0){
+            sum++;
+        }
+    }
+    if(sum==2){
+        return true;
+    }else{
+        return false;
+    }
+}
+function isTrue(a){
+    for(var i=2;i<a-2;i++){
+        var j = a-i;
+        if(goldbach(i)&&goldbach(j)){
+            document.write(a+"可以拆分为两个质数"+i+"与"+j+"的和");
+        }
+    }
+}
