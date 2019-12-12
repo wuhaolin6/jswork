5af784df520c080bceca41cc9ce237354299905a
wuhaolin6 <364641104@qq.com>
2019-12-12 13:46:11 +0800

    work16

--- work16/index1.js
+++ work16/index1.js
@@ -29,3 +29,7 @@ var obj = document.getElementById('box');
 obj.onclick = function() {
     animate(obj,{'left':200, 'top':50});
 };
+var obj=document.getElementById('box');
+obj.onclick=function() {
+    animate(obj,{'left':Math.random()*1900,'top':Math.random()*930});
+};
